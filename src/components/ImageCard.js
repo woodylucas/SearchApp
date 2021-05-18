3b9b2983081d3aec887708caa4f380a291139380
woodylucas <wlucas0311@gmail.com>
2021-05-18 18:13:41 -0400

    refactored: css grid with react

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,5 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default function ImageCard() {
-  return <div></div>;
+class ImageCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { spans: 0 };
+    this.setSpans = this.setSpans.bind(this);
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener("load", this.setSpans);
+  }
+
+  setSpans() {
+    const height = this.imageRef.current.clientHeight;
+    const spans = Math.ceil(height / 10);
+    this.setState({ spans });
+  }
+
+  render() {
+    const { descriptiion, urls } = this.props.image;
+    return (
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        <img ref={this.imageRef} alt={descriptiion} src={urls.regular} />
+      </div>
+    );
+  }
 }
+
+export default ImageCard;
--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "./ImageList.css";
+import ImageCard from "./ImageCard";
 
 export default function ImageList({ images }) {
-  const renderImages = images.map(({ description, id, urls }) => (
-    <img key={id} src={urls.regular} alt={description} />
+  const renderImages = images.map((image) => (
+    <ImageCard key={image.id} image={image} />
   ));
   return <div className="image-list">{renderImages}</div>;
 }
